c91b4335794c1ca2facfc1f92bb511e39ff38966
mattnot3 <70361629+mattnot3@users.noreply.github.com>
2020-08-31 17:57:45 +0200

    Update exSW3.py
    Aggiunto controllo errori più specifico

--- TIOT GITHUB/SW/SW1/exSW3.py
+++ TIOT GITHUB/SW/SW1/exSW3.py
@@ -8,9 +8,16 @@ class Convertor():
         
         body = cherrypy.request.body.read()
         json_body = json.loads(body.decode('utf-8'))
+        
+        if (not(('originalUnit' in json_body) and ('targetUnit' in json_body) and ('values' in json_body))):
+            return 'Wrong name of params: they should be "originalUnit", "targetUnit" and "values".'
+
         originalUnit = json_body['originalUnit']
         targetUnit = json_body['targetUnit']
         
+        if(len(json_body)!=3):
+            return 'Wrong number of params.'
+
         fout = open("convertor.json", "w")
         i=0
         newValues = []
@@ -22,18 +29,24 @@ class Convertor():
                     newValues.append(originalValue + 273.15)
                 elif(targetUnit == 'F'):
                     newValues.append((originalValue * 9/5) + 32)
+                else:
+                    return 'Wrong param value: use "C", "K" or "F".'
             elif(originalUnit == 'K'):
                 if(targetUnit == 'C'):
                     newValues.append(originalValue - 273.15)
                 elif(targetUnit == 'F'):
                     newValues.append(((originalValue - 273.15) * 9/5) + 32)
+                else:
+                    return 'Wrong param value: use "C", "K" or "F".'
             elif(originalUnit == 'F'):
                 if(targetUnit == 'C'):
                     newValues.append((originalValue - 32) * 5/9)
-                if(targetUnit == 'K'):
+                elif(targetUnit == 'K'):
                     newValues.append((originalValue - 32) * 5/9 + 273.15)
+                else:
+                    return 'Wrong param value: use "C", "K" or "F".'
             else:
-                newValues = 'error'
+                return 'Wrong param value: use "C", "K" or "F".'
             
             json_body['newValues'] = newValues
     
@@ -46,7 +59,7 @@ if __name__ == '__main__':
     conf={
             '/':{
                 'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-                'tool.session.on': True
+                'tools.sessions.on': True
                 }
     }
     cherrypy.tree.mount(Convertor(), '/', conf)
