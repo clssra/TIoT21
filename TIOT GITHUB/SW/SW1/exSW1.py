70af87bbd3f921c5bdb2c0dbf270b1a1f4fd0e66
mattnot3 <70361629+mattnot3@users.noreply.github.com>
2020-08-31 17:26:31 +0200

    Update exSW1.py
    Aggiunto controllo errori più specifico (come sembra richiedere il testo)

--- TIOT GITHUB/SW/SW1/exSW1.py
+++ TIOT GITHUB/SW/SW1/exSW1.py
@@ -7,27 +7,36 @@ class Convertor():
     def GET(self, *uri, **params):
         
         fout = open("convertor.json", "w")
-
+        if (not(('originalUnit' in params) and ('targetUnit' in params) and ('value' in params))):
+            return 'Wrong name of params: they should be "originalUnit", "targetUnit" and "value".'
         originalUnit = params['originalUnit']
         targetUnit = params['targetUnit']
         originalValue = float(params['value'])
+        if(len(params)!=3):
+            return 'Wrong number of params.'
         if(originalUnit == 'C'):
             if(targetUnit == 'K'):
                 newValue = originalValue + 273.15
             elif(targetUnit == 'F'):
                 newValue = (originalValue * 9/5) + 32
+            else:
+                return 'Wrong param value: use "C", "K" or "F".'
         elif(originalUnit == 'K'):
             if(targetUnit == 'C'):
                 newValue = originalValue - 273.15
             elif(targetUnit == 'F'):
                 newValue = ((originalValue - 273.15) * 9/5) + 32
+            else:
+                return 'Wrong param value: use "C", "K" or "F".'
         elif(originalUnit == 'F'):
             if(targetUnit == 'C'):
                 newValue = (originalValue - 32) * 5/9
-            if(targetUnit == 'K'):
+            elif(targetUnit == 'K'):
                 newValue = (originalValue - 32) * 5/9 + 273.15
+            else:
+                return 'Wrong param value: use "C", "K" or "F".'
         else:
-            newValue = 'error'
+            return 'Wrong param value: use "C", "K" or "F".'
         
         params['newValue'] = newValue
         
@@ -41,7 +50,7 @@ if __name__ == '__main__':
     conf={
             '/':{
                 'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-                'tool.session.on': True
+                'tools.sessions.on': True
                 }
     }
     cherrypy.tree.mount(Convertor(), '/', conf)
